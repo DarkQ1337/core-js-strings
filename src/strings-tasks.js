a90db4aff17b69df3ef6daf4d8fd30b081912f72
DarkQ1337 <yasyaklas@gmail.com>
2023-11-20 17:04:22 +0600

    fix: 2/10

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,12 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(param) {
+  if (typeof param !== 'string') {
+    return 0;
+  }
+  const strLength = param.length;
+  return strLength;
 }
 
 /**
@@ -37,8 +41,11 @@ function getStringLength(/* value */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(param) {
+  if (typeof param === 'string' || param instanceof String) {
+    return true;
+  }
+  return false;
 }
 
 /**
