8a3862047aeb31ea7f0d4efdc56ef3a4fa6fb141
DarkQ1337 <yasyaklas@gmail.com>
2023-11-20 17:45:11 +0600

    14 / 29

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -137,8 +137,11 @@ function removeTrailingWhitespaces(param) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(param1, param2) {
+  if (param2 >= 0) {
+    return param1.repeat(param2);
+  }
+  return '';
 }
 
 /**
